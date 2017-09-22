eebc4e5b962aa819cd5c18949a494c80a86e3ad2
Per Wikman <Per.Wikman@pensionsmyndigheten.se>
2017-09-22 16:58:51 +0200

    update filert example

--- src/minimalbar.js
+++ src/minimalbar.js
@@ -10,6 +10,7 @@
 // Note: It is often a good idea to have these objects accessible at the global scope so that they can be modified or
 // filtered by other page controls.
 var smallChart = dc.barChart('#small-chart');
+var select1 = dc.selectMenu('#select1');
 
 
 //### Load your data
@@ -26,8 +27,10 @@ var smallChart = dc.barChart('#small-chart');
 
     //See the [crossfilter API](https://github.com/square/crossfilter/wiki/API-Reference) for reference.
     var ndx = crossfilter(data);
-    var dim = ndx.dimension(function (d) { return d["type"]; })
-    var grp = dim.group().reduceSum(function (d) { return d["open"]; })
+    var dim1 = ndx.dimension(function (d) { return d["type"]; })
+    var grp1 = dim1.group().reduceSum(function (d) { return d["open"]; })
+    var dim2 = ndx.dimension(function (d) { return d["type"]; })
+    var grp2 = dim2.group().reduceSum(function (d) { return d["open"]; })
     var all = ndx.groupAll();
 
     // Dimension by year
@@ -48,18 +51,18 @@ var smallChart = dc.barChart('#small-chart');
     //    return Math.abs(d.close - d.open);
     //});
    
-
     // -------------------------------- charts -----------------------------
 
     // #### some fidgeting with a barchart
     // life saver many thanks https://github.com/dc-js/dc.js/wiki/FAQ#remove-empty-bins
+    var byNameSyncGroup = [];
 
     smallChart /* dc.barChart('#volume-month-chart', 'chartGroup') */
     .width(990)
     .height(180)
-    .margins({top: 10, right: 50, bottom: 30, left: 40})
-    .dimension(dim)
-    .group(grp)
+    .margins({top: 10, right: 50, bottom: 30, left: 40})    
+    .dimension(dim1)
+    .group(grp1)
     .elasticY(true)
     .elasticX(true)
     // (_optional_) whether bar should be center to its x value. Not needed for ordinal chart, `default=false`
@@ -79,6 +82,14 @@ var smallChart = dc.barChart('#small-chart');
     .xUnits(dc.units.ordinal)
     .renderHorizontalGridLines(true);
     // Customize the filter displayed in the control span
+
+
+    select1
+    .dimension(dim2)
+    .group(grp2)
+    .multiple(true)
+    .numberVisible(10)
+    .controlsUseVisibility(true);
 
     //#### Rendering
 
